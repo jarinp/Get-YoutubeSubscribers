4472960667dea503c0a5bdf2f2bcb9fb28b10665
prasanth-krp <prasanthkrofficial@gmail.com>
2024-08-08 19:27:57 +0530

    port variable declaration changed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = require('./app.js')
 const mongoose = require('mongoose')
-const port = 10000
+const port = process.env.PORT || 10000;
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json())
