5c553d8c0cee681d87ce33c09dda9aa1140508f9
jarinp <jerinr050@gmail.com>
2024-08-09 15:04:39 -0800

    message

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 app.get('/subscribers', async (req, res) => {
     try {
         const subscribers = await subscriberModel.find();
-        res.json(subscribers);
+        res.status(200).json(subscribers);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -27,7 +27,7 @@ app.get('/subscribers', async (req, res) => {
 app.get('/subscribers/names', async (req, res) => {
     try {
         const subscribers = await subscriberModel.find({}, { name: 1, subscribedChannel: 1, _id: 0 });
-        res.json(subscribers);
+        res.status(200).json(subscribers);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -40,7 +40,7 @@ app.get('/subscribers/:id', async (req, res) => {
         if (!subscriber) {
             return res.status(404).json({ message: 'Subscriber not found' });
         }
-        res.json(subscriber);
+        res.status(200).json(subscriber);
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
